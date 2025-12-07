3876cc35b06b452e761b759fcf195244d39902af
Robin Erike <robinerike@gmail.com>
2025-12-07 11:58:37 +0100

    renderung users

--- src/app/main-content/workspace/workspace.ts
+++ src/app/main-content/workspace/workspace.ts
@@ -3,8 +3,11 @@ import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { CreateChannel } from './create-channel/create-channel';
 
-import { Channel, FirestoreService } from '../../services/firestore.service';
-import { AuthService } from '../../services/auth.service';
+import {
+  Channel,
+  DirectMessage,
+  FirestoreService,
+} from '../../services/firestore.service';import { AuthService } from '../../services/auth.service';
 import { User } from '@angular/fire/auth';
 import { ChannelSelectionService } from '../../services/channel-selection.service';
 
@@ -19,7 +22,10 @@ export class Workspace {
   private readonly firestoreService = inject(FirestoreService);
   private readonly authService = inject(AuthService);
     private readonly channelSelectionService = inject(ChannelSelectionService);
-  protected readonly channels$: Observable<Channel[]> = this.firestoreService.getChannels();
+  protected readonly channels$: Observable<Channel[]> =
+    this.firestoreService.getChannels();
+  protected readonly directMessages$: Observable<DirectMessage[]> =
+    this.firestoreService.getDirectMessages();
   protected readonly currentUser$: Observable<User | null> = this.authService.user$;
     protected readonly selectedChannelId$ =
     this.channelSelectionService.selectedChannelId$;
